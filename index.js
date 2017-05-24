fbabb73bb00dd986d13c38cbd5d36a4122068d1c
Anton Spaskyi <asp@kyiv.ciklum.net>
2017-05-24 17:22:35 +0300

    index file

---
+++ index.js
@@ -0,0 +1,28 @@
+var p1 = new Promise((resolve, reject) => (
+	setTimeout(() => {
+			return resolve(
+				 [{name: 'name1'}, {name: 'name2'}]
+			)
+	}, 2000)
+));
+
+p1.then(results => {
+	const names = results;
+	console.log(results);
+	results.forEach((response, index) => (
+		new Promise((resolve, reject) => (
+			setTimeout(() => {
+					let res;
+					if (response.name === 'name1') {
+							res = 'name4';
+					} else {
+							res = 'name5';
+					}
+
+					resolve({name: res});
+			}, 2000)
+		)).then(result => {
+			names[index].name = result.name;
+		})
+	));
+});
